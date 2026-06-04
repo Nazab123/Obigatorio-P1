3b06bc2718d9feef41704bad9c6ab7183a656039
Gerard Figueroa <109176257+Gerard-7@users.noreply.github.com>
2026-06-04 20:00:54 -0300

    nao nao

--- js/clases/sistema.js
+++ js/clases/sistema.js
@@ -21,8 +21,42 @@ class Sistema {
         this.ofertas = [];
         this.postulaciones = [];
     }
+
+    //pregarca de datos va aca 
+
+
+    // aca iria la precarga de datos 
 }
 
+
+function indexOfAdmin(txtUsuarioLogin) {
+
+    for (let i = 0; i < sistema.admins.length; i++) {
+        let adminActual = sistema.admins[i];
+
+        if (adminActual.usuario === txtUsuarioLogin) {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+function indexOfPostulante(txtUsuarioLogin) {
+
+    for (let i = 0; i < sistema.postulantes.length; i++) {
+        let postulanteActual = sistema.postulantes[i];
+
+        if (postulanteActual.usuario === txtUsuarioLogin) {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+//--------------------------------------------------------------
+//datos de la precarga de datos 
 let sistema = new Sistema();
 let admin1 = new Admin("adminrodri", "Rodri123", "Rodri");
 let admin2 = new Admin("admingerard", "Gerard123", "El Gerry");
@@ -36,13 +70,98 @@ sistema.admins.push(admin2);
 sistema.admins.push(admin3);
 
 
+//datos de la precarga de datos
+//--------------------------------------------------------------
 /* yo voy a teneer en el main todo lo de html, nada se conecta con el hml menossss el main, las vitas y todo van en el ,ain mi clase sistema habla con el main 
 */
 
 
+/* VALIDACIONES  */
+ let posicionAdmin = indexOfAdmin(txtUsuarioLogin);
+    let posicionPostulante = indexOfPostulante(txtUsuarioLogin);
+
+    // acá valido si es admin o postulante
+    if (posicionAdmin !== -1) {
+
+        let adminEncontrado = sistema.admins[posicionAdmin];
+
+        if (adminEncontrado.password === txtPasswordLogin) {
+            respuestaLogin = "Login correcto como administrador";
+            //aca te voy a agregar para las pantallas 
+            mostrarPantalla("pantallaAdmin");
+        } else {
+            respuestaLogin = "Contraseña incorrecta";
+        }
+    } else if (posicionAdmin === -1 && posicionPostulante !== -1) {
+
+        let postulanteEncontrado = sistema.postulantes[posicionPostulante];
+
+        if (postulanteEncontrado.password === txtPasswordLogin) {
+            respuestaLogin = "Login correcto como postulante";
+            //aca igual .
+            mostrarPantalla("pantallaPostulante");
+        } else {
+            respuestaLogin = "Contraseña incorrecta";
+        }
+
+    } else {
+        respuestaLogin = "nao nao";
+    }
 
 
+/* funccion registrar postulante */
 
+let resultadoContra = validarContra(password);
+
+    if (usuario === "") {
+        respuesta += "el usuario no debe estar vacio<br>";
+
+    } if (usuario.length < 5) {
+        respuesta += "el usuario debe tener al menos 5 caracteres<br>";
+
+    } if (password === "") {
+        respuesta += "la contraseña no debe estar vacia<br>";
+
+    } if (password.length < 5) {
+        respuesta += "la contraseña debe tener al menos 5 caracteres<br>";
+
+    } if (nombre.length < 5) {
+        respuesta += "el nombre debe tener al menos 5 caracteres<br>";
+
+    } if (experiencia === "") {
+        respuesta += "seleccione una opcion de experiencia<br>";
+
+    } if (area === "") {
+        respuesta += "seleccione una opcion de area<br>";
+
+    } if (indexOfAdmin(usuario) !== -1 || indexOfPostulante(usuario) !== -1) {
+        respuesta += "el usuario ya existe<br>";
+
+    } if (resultadoContra.contadorNumPas === 0) {
+        respuesta += "la contraseña debe tener al menos un numero<br>";
+
+    } if (resultadoContra.tieneMayus === false) {
+        respuesta += "la contraseña debe tener al menos una mayuscula<br>";
+
+    } if (resultadoContra.tieneMinus === false) {
+        respuesta += "la contraseña debe tener al menos una minuscula<br>";
+
+    }
+
+    if (respuesta === "") {
+
+        let nuevoPostulante = new Postulante(
+            usuario,
+            password,
+            nombre,
+            experiencia,
+            area
+        );
+
+        sistema.postulantes.push(nuevoPostulante);
+
+        respuesta = "registro procesado";
+    }
 
 
 
@@ -79,6 +198,13 @@ let contadorNumPas = 0;
     tieneMinus,
     contadorNumPas
     }
+
+
+
+
+
+
+
 
     agregarOferta()
     agregarPostulacion()
